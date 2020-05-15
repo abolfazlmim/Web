89485263810c69950cf93fcbbbe786ae55f409a1
Abolfazl <afmmyam@gmail.com>
2020-05-15 07:00:34 -0400

    users added

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -19,4 +19,5 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("flights/" , include("flights.urls")),
+    path("users/" , include("users.urls")),
 ]
